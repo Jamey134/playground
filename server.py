11a359556fdd9fedad89b632938d956e61b1c6b6
James Watson <jameywatson134@gmail.com>
2023-06-10 18:23:28 -0700

    updated commit

--- server.py
+++ server.py
@@ -1,37 +1,27 @@
-# from flask import Flask, render_template
-# app = Flask(__name__)
-
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
 from flask import Flask, render_template
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return render_template("test.html", phrase="hello", times=5)	# notice the 2 new named arguments!
-if __name__=="__main__":
-    app.run(debug=True)
+
+
+
+@app.route('/play')
+def play():
+    return render_template('index.html')
+
+
+@app.route('/play/<int:x>')
+def play_x(x):
+    return render_template('index_2.html', x = x)
+
+
+@app.route('/play/<int:x>/<string:color>')
+def play_x_color(x, color):
+    return render_template('index_3.html', x = x, color = color)
 
 
 
 
-@app.route('/hello')
-
-def hello_world():
-    return'Hello World'
-
-@app.route('/hello/<name>')
-def hello_name(name):
-    return f'Hello {name}'
-
-
-@app.route('/hello/<name>/<food>')
-def hello_name_food(name, food):
-    return f'Hello {name}, my favorite food is: {food}'
 
 
 
 if __name__=="__main__":
-        app.run( debug=True, port=5001 )
+    app.run( debug=True, port=5001 )
